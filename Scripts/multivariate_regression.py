5ae7a3bd08fcc842148e2b48bfe98432e088c152
Yuan <yiy046@nc6.ucsd.edu>
2026-04-19 12:40:39 -0700

    prediction falls back on past years data so more counties are present, tho not by a lot

--- Scripts/multivariate_regression.py
+++ Scripts/multivariate_regression.py
@@ -73,59 +73,59 @@ model = smf.ols(formula=formula, data=df_centered).fit()
 print(model.summary())  
 
 
-# #########################################################################################################
-# # Test prediction and visualize the regression goodness of fit
-# #########################################################################################################
+#########################################################################################################
+# Test prediction and visualize the regression goodness of fit
+#########################################################################################################
 
-# # Create new column with the predicted ED rates
-# df_final = df_final.copy()
-# df_final['predicted_er_rate'] = np.nan
+# Create new column with the predicted ED rates
+df_final = df_final.copy()
+df_final['predicted_er_rate'] = np.nan
 
-# # Split data and CV when running model
-# kf = KFold(n_splits=20, shuffle=True, random_state=411)
-# fold_r2 = []
-# fold_mse = []
-# for train_index, test_index in kf.split(df_final):
+# Split data and CV when running model
+kf = KFold(n_splits=20, shuffle=True, random_state=411)
+fold_r2 = []
+fold_mse = []
+for train_index, test_index in kf.split(df_final):
     
-#     train_data = df_final.iloc[train_index]
-#     test_data = df_final.iloc[test_index]
+    train_data = df_final.iloc[train_index]
+    test_data = df_final.iloc[test_index]
 
-#     fold_model = smf.ols(formula=formula, data=train_data).fit()
+    fold_model = smf.ols(formula=formula, data=train_data).fit()
     
-#     # Save predictions
-#     predicted = fold_model.predict(test_data)
-#     df_final.loc[df_final.index[test_index], 'predicted_er_rate'] = predicted
+    # Save predictions
+    predicted = fold_model.predict(test_data)
+    df_final.loc[df_final.index[test_index], 'predicted_er_rate'] = predicted
 
-#     # Save metrics for current fold
-#     r2 = r2_score(test_data['AGE_ADJUSTED_ED_VISIT_RATE'], predicted)
-#     mse = mean_squared_error(test_data['AGE_ADJUSTED_ED_VISIT_RATE'], predicted)
-#     fold_r2.append(r2)
-#     fold_mse.append(mse)
-#     print(f"This fold: R² = {r2:.3f}, MSE = {mse:.3f}\n")
+    # Save metrics for current fold
+    r2 = r2_score(test_data['AGE_ADJUSTED_ED_VISIT_RATE'], predicted)
+    mse = mean_squared_error(test_data['AGE_ADJUSTED_ED_VISIT_RATE'], predicted)
+    fold_r2.append(r2)
+    fold_mse.append(mse)
+    print(f"This fold: R² = {r2:.3f}, MSE = {mse:.3f}\n")
 
-# # Average and spread of model fit metrics
-# mean_r2 = np.mean(fold_r2)
-# std_r2 = np.std(fold_r2)
-# mean_mse = np.mean(fold_mse)
-# std_mse = np.std(fold_mse)
-# print("============= Final CV Performance =============")
-# print(f"Mean R²:  {mean_r2:.3f} ± {std_r2:.3f}")
-# print(f"Mean MSE: {mean_mse:.3f} ± {std_mse:.3f}")
+# Average and spread of model fit metrics
+mean_r2 = np.mean(fold_r2)
+std_r2 = np.std(fold_r2)
+mean_mse = np.mean(fold_mse)
+std_mse = np.std(fold_mse)
+print("============= Final CV Performance =============")
+print(f"Mean R²:  {mean_r2:.3f} ± {std_r2:.3f}")
+print(f"Mean MSE: {mean_mse:.3f} ± {std_mse:.3f}")
 
-# # Visualize results across all folds
-# plt.figure(figsize=(8, 6))
-# sns.scatterplot(data=df_final, x='AGE_ADJUSTED_ED_VISIT_RATE', y='predicted_er_rate', hue='AGE')
+# Visualize results across all folds
+plt.figure(figsize=(8, 6))
+sns.scatterplot(data=df_final, x='AGE_ADJUSTED_ED_VISIT_RATE', y='predicted_er_rate', hue='AGE')
 
-# line_min = min(df_final['AGE_ADJUSTED_ED_VISIT_RATE'].min(), df_final['predicted_er_rate'].min())
-# line_max = max(df_final['AGE_ADJUSTED_ED_VISIT_RATE'].max(), df_final['predicted_er_rate'].max())
-# plt.plot([line_min, line_max], [line_min, line_max], color='red', linestyle='--', label='Ideal Perfect Fit')
+line_min = min(df_final['AGE_ADJUSTED_ED_VISIT_RATE'].min(), df_final['predicted_er_rate'].min())
+line_max = max(df_final['AGE_ADJUSTED_ED_VISIT_RATE'].max(), df_final['predicted_er_rate'].max())
+plt.plot([line_min, line_max], [line_min, line_max], color='red', linestyle='--', label='Ideal Perfect Fit')
 
-# plt.title(f"Cross-Validated Model Fit: Actual vs. Predicted (5 Folds)\n Mean R2 = {mean_r2}, Mean MSE = {mean_mse}")
-# plt.xlabel("Actual ER Visit Rate (Demeaned)")
-# plt.ylabel("Predicted ER Visit Rate")
-# plt.legend()
-# plt.savefig(f'{out_dir}/cv_prediction.png')
-# plt.show()
+plt.title(f"Cross-Validated Model Fit: Actual vs. Predicted (5 Folds)\n Mean R2 = {mean_r2}, Mean MSE = {mean_mse}")
+plt.xlabel("Actual ER Visit Rate (Demeaned)")
+plt.ylabel("Predicted ER Visit Rate")
+plt.legend()
+plt.savefig(f'{out_dir}/cv_prediction.png')
+plt.show()
 
 
 
@@ -134,7 +134,7 @@ print(model.summary())
 #########################################################################################################
 
 # Load 2025 air pollution data
-new_air_data = pd.read_csv(f'{root_dir}/EPA_data/air_aqi_and_particles_annual/2025.csv')
+new_air_data = pd.read_csv(f'{root_dir}/EPA_data/air_aqi_and_particles_annual/2023-2025.csv')
 
 # Make sure to preprocess this dataset the same way we did when training model to maintain consistency
 # and same interpretability, including demeaning each pollutant by within its county.
@@ -154,12 +154,28 @@ for particle in select_particles:
 # Drop helper columns used for demeaning, the historical content
 new_df_centered = new_df_centered.drop(columns=[f'{p}_hist' for p in select_particles])
 
-print(new_df_centered)
-
 # Still lots of NaNs, impute missing values with the mean of pollutant concentration in that county across years
+# If missing, find the most recent year in the file. Still this doesn't save states with no data in the past e
+# years so they will just have to be empty. 
+melted = new_df_centered.melt(
+    id_vars=['year', 'county'], 
+    value_vars=select_particles, 
+    var_name='particle', 
+    value_name='avg_val'
+)
+melted = melted.dropna(subset=['avg_val'])
+idx = melted.groupby(['county', 'particle'])['year'].idxmax()
+latest_data_points = melted.loc[idx]
+new_df_final = latest_data_points.pivot(
+    index='county', 
+    columns='particle', 
+    values='avg_val'
+).reset_index()
+new_df_final['reference_year'] = latest_data_points.groupby('county')['year'].max().values
+
 print(f"After data imputation, still have to drop {new_df_centered.isna().any(axis=1).sum()} entries due to one or more"
         f"missing pollutant data.") 
-new_df_final = new_df_centered.dropna()
+new_df_final = new_df_final.dropna()
 
 # Add new column of age group so model can predict accordingly
 new_df_final['AGE'] = 'child'

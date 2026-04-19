7f0594f3af6538cdf6adb06bb6ae988c294cf3d6
Yuan <yiy046@nc4.ucsd.edu>
2026-04-19 08:15:19 -0700

    modified linear regression with demeaning and cleaner output

--- Scripts/simple_regression.py
+++ Scripts/simple_regression.py
@@ -4,13 +4,14 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import statsmodels.api as sm
 from sklearn.linear_model import LinearRegression
 
 #########################################################################################################
 # Load data, merge and run simple linear regression
 #########################################################################################################
 
-# Loaction of daily detailed particle datasets (in my local directory, not in github)
+# Data directories for load and save
 root_dir = '/mnt/neurocube/local/serenceslab/Stella/misc/Data_Hacks_2026'
 out_dir = '/mnt/neurocube/local/serenceslab/Stella/misc/Data_Hacks_2026/Figs'
 
@@ -44,24 +45,43 @@ for particle in particles:
         columns='particle', 
         values='avg_val'
     ).reset_index()
-                            
-    print(df_wide)
 
     # Start running regression across age groups
     for name, group in df_wide.groupby('AGE'):
-        model = LinearRegression()
         
-        X = group[select_particle]
-        y = group['AGE_ADJUSTED_ED_VISIT_RATE']
-        
-        model.fit(X, y)
-        
-        print(f"Age Group: {name}")
-        for col, coef in zip(select_particle, model.coef_):
-            print(f"  {col} Coefficient: {coef:.4f}")
-        print(f"  Intercept: {model.intercept_:.4f}")
-        print(f"  R-squared: {model.score(X, y):.4f}")  
+        # Realized that since the county effect is probably large for some pollutants, and we cannot include county as a regressor
+        # due to only 1 datapoint per age group per county, we have to implicitly remove its effect by mean-normalizing the pollutant
+        # concentration within each county, then run regression. So slope interpretation is now: with every additional unit of pollutant
+        # concentration with respect to the mean of a given county, what's the change in ED visit rate with respect to the mean in that
+        # same county. 
 
+        group['y_demeaned'] = group['AGE_ADJUSTED_ED_VISIT_RATE'] - group.groupby('county')['AGE_ADJUSTED_ED_VISIT_RATE'].transform('mean')
+        group['X_demeaned'] = group[select_particle] - group.groupby('county')[select_particle].transform('mean')
+        group['X_mean'] = group.groupby('county')[select_particle].transform('mean') # also save the mean particle concentration itself
+
+        X = group['X_demeaned']
+        y = group['y_demeaned']
+
+        # For demeaned data, intercept will be 0 so no need to include, but let's just have it here anyways in case we want to
+        # go back to using the original raw data
+        X = sm.add_constant(X)
+        sm_model = sm.OLS(y, X).fit()
+
+        slope = sm_model.params.iloc[1]
+        pval = sm_model.pvalues.iloc[1]
+        R2 = sm_model.rsquared
+        
+        if pval < 0.05: print(f"!!! Signicant !!!")
+        print(f"{select_particle[0]}")
+        print(f"\tAge Group: {name}")
+        print(f"\t\tSlope: {slope:.4f}")
+        print(f"\t\tP-value: {pval: .4f}")
+        print(f"\t\tR-squared: {R2:.4f}")
+        print(f"\t\tNum Datapoints: {len(y)}\n")
+
+        # An overview of the average pollutant concentration across counties 
+        print(f"\t\tMean Pollutant Concentration: {group[['county', 'X_mean']].drop_duplicates()}\n\n")
+        print("="*70)
 
 
 
